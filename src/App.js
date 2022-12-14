d193e7ee5265769201c0ec9855688392ee1eefc1
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-12-14 16:41:31 +1100

    feat: fetch data and add data to my components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
 import coldBg from "./assets/cold.jpg";
 import hotBg from "./assets/hot.jpg";
+import Description from "./components/Description";
+import {useEffect, useState} from "react";
+import{getFormattedWeatherData} from "./weatherService";
+
 
 function App() {
+    const [weather, setWeather] = useState(null);
+    const [units, setUnits] = useState('imperial');
+
+    useEffect(()=>{
+        const fetchWeatherData = async()=>{
+            const data = await getFormattedWeatherData("paris", units);
+            //console.log(data); shows what we fetched in an object
+            setWeather(data);
+        };
+        fetchWeatherData();
+    },[])
+
+
     return (
         <div className="app" style={{backgroundImage: `url(${coldBg})`}}>
             <div className="overlay">
-                <div className="container">
-                    {/*search input part*/}
-                    <div className="section section__inputs">
-                        <input type='text' name='city' placeholder="Search the City"></input>
-                        <button> °F</button>
-                    </div>
 
-                    <div className='section section__temperature'>
-                        {/*weather icon*/}
-                        <div className="icon">
-                            <h3>London, GB</h3>
-                            <img src="https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-256.png"
-                                 alt="weather icons"></img>
-                            <h3>Sunny</h3>
-                        </div>
-                        {/*temperature div*/}
-                        <div className="temperature">
-                            <h1>28°C</h1>
+                {/*only if weather exists, then render the components.*/}
+                {weather && (
+                    <div className="container">
+                        {/*search input part*/}
+                        <div className="section section__inputs">
+                            <input type='text' name='city' placeholder="Search the City"></input>
+                            <button> °F</button>
                         </div>
 
+                        <div className='section section__temperature'>
+                            {/*weather icon*/}
+                            <div className="icon">
+                                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                                <img src={weather.iconURL} alt="weather icons"></img>
+                                <h3>{weather.description}</h3>
+                            </div>
+                            {/*temperature div*/}
+                            <div className="temperature">
+                                <h1>{`${weather.temp.toFixed()} °${units === 'metric'? 'C' : 'F'} `}</h1>
+                            </div>
+                        </div>
+
+                        {/*bottom description*/}
+                        <Description weather={weather} units = {units}/>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     );
